2c554bea4bd974252a1915688508bf419cab32e0
setxand <setxand@gmail.com>
2019-12-22 16:15:51 +0200

    Minor updates

--- src/main/java/com/mafia/service/ServiceValidator.java
+++ src/main/java/com/mafia/service/ServiceValidator.java
@@ -3,11 +3,15 @@ package com.mafia.service;
 import com.mafia.exceprion.BotException;
 import com.mafia.model.Room;
 import com.mafia.model.User;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import telegram.Message;
 
 @Component
 public class ServiceValidator {
+
+	@Value("app.mode") private Boolean testMode;
 
 	private final RoomService roomService;
 
@@ -26,7 +30,7 @@ public class ServiceValidator {
 	}
 
 	public void validateStartGame(User user, Room room, Message message) {
-		if (room.getUsers().size() < 5) throw new BotException(message, "Minimum players is 5!");
+		if (room.getUsers().size() < 5 && !testMode) throw new BotException(message, "Minimum players is 5!");
 
 		if (!user.getChatId().equals(room.getHostId()))
 			throw new BotException(message, "You are not a host of the game");
